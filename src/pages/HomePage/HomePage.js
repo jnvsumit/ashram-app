05f30b7f6b12d8d52c1b1ea2ec2f78cc5760a59b
Madan Gopal <madan.23.2@mountblue.tech>
2023-05-12 17:31:15 +0530

    feat: Added book carousel feature to homepage

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,59 +1,94 @@
-import React, {useContext, useEffect, useState} from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Modal from "react-modal";
 import { useNavigate } from "react-router-dom";
 import AddBookCard from "../../components/BookCard/AddBookCard";
 import BookCard from "../../components/BookCard/BookCard";
-import Carousel from "../../components/carousel/Carousel";
+import HomePageCarousel from "./HomePageCarousel";
 import withLoading from "../../components/HOC/WithLoading";
 import { BookCardContext } from "../../context/BookCardContextProvider";
 import { UserContext } from "../../context/UserContextProvider";
 import AddBookModal from "../../components/popup/AddBookModal";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 import {
-    Container,
-    Main,
-    CarouselSection,
-    BooksArticlesSection,
-    CardContainer,
-    ContextSection, VideoSection,
+  Container,
+  Main,
+  CarouselSection,
+  BooksArticlesSection,
+  CardContainer,
+  ContextSection, VideoSection,
 } from "./HomePage.styles";
-import {LoadingContext} from "../../context/LoadingContextProvider";
-import {BookContext} from "../../context/BookContextProvider";
+import { LoadingContext } from "../../context/LoadingContextProvider";
+import { BookContext } from "../../context/BookContextProvider";
 import CustomVideo from "../../components/Videos/CustomVideo";
+import { CardGroup } from "react-bootstrap";
 
-const Images = [
-    "https://picsum.photos/id/1018/800/400",
-    "https://picsum.photos/id/1020/800/400",
-    "https://picsum.photos/id/1021/800/400",
-    "https://picsum.photos/id/1022/800/400",
-];
+const settings = {
+  dots: true,
+  infinite: false,
+  speed: 500,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  cssEase: "linear",
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 760,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        initialSlide: 2,
+        infinite:true,
+        dots:true
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+};
+
 
 const HomePage = () => {
-    const navigate = useNavigate();
-    const { bookCard, setPageNumber } = useContext(BookCardContext);
-    const { onSave, onDelete } = useContext(BookContext);
-    const { user } = useContext(UserContext);
-    const { setLoading } = useContext(LoadingContext);
-    const [isAddBookModalOpen, setIsAddBookModalOpen] = useState(false);
+  const navigate = useNavigate();
+  const { bookCard, setPageNumber } = useContext(BookCardContext);
+  const { onSave, onDelete } = useContext(BookContext);
+  const { user } = useContext(UserContext);
+  const { setLoading } = useContext(LoadingContext);
+  const [isAddBookModalOpen, setIsAddBookModalOpen] = useState(false);
 
-    const handleAddBookSubmit = async ({ title, authors, thumbnail, description }) => {
-        setLoading(true);
+  const handleAddBookSubmit = async ({ title, authors, thumbnail, description }) => {
+    setLoading(true);
 
-        const formData = new FormData();
-        formData.append("title", title);
-        formData.append("authors", JSON.stringify(authors));
-        formData.append("thumbnail", thumbnail);
-        formData.append("description", description);
+    const formData = new FormData();
+    formData.append("title", title);
+    formData.append("authors", JSON.stringify(authors));
+    formData.append("thumbnail", thumbnail);
+    formData.append("description", description);
 
-        try {
-            const bookId = await onSave(formData);
-            navigate(`/books/${bookId}`);
-        } catch (error) {
-            console.error("Error adding book:", error);
-        } finally {
-            setLoading(false);
-        }
-    };
+    try {
+      const bookId = await onSave(formData);
+      navigate(`/books/${bookId}`);
+    } catch (error) {
+      console.error("Error adding book:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   const handleAddBookCardClick = () => {
     setIsAddBookModalOpen(true);
@@ -67,48 +102,47 @@ const HomePage = () => {
     <Container>
       <Main>
         <CarouselSection>
-          <Carousel images={Images}/>
+          <HomePageCarousel />
         </CarouselSection>
         <BooksArticlesSection>
           <h2>Featured Books & Articles</h2>
           <CardContainer>
             {
-              bookCard.books.map((book, index) => (
-                    <BookCard
-                        key={index}
-                        bookId={book.bookId}
-                        userId={book.userId}
-                        title={book.title}
-                        authors={book.authors}
-                        description={book.description}
-                        thumbnail={book.thumbnail}
-                        onDelete={onDelete}
-                    />
-                ))
+              console.log(bookCard.books)
             }
-            {user.isLoggedIn && <AddBookCard onClick={handleAddBookCardClick} />}
-              <AddBookModal
-                  isOpen={isAddBookModalOpen}
-                  onRequestClose={() => setIsAddBookModalOpen(false)}
-                  onSubmit={handleAddBookSubmit}
-                  ariaHideApp={false}
-              />
+            <Slider {...settings} beforeChange={(current, next) => {
+              console.log(`Current slide index: ${current}, Next slide index: ${next}`);
+            }}>
+              {bookCard.books.map((book, index) => (
+                <div key={index}>
+                  <BookCard
+                    bookId={book.bookId}
+                    userId={book.userId}
+                    title={book.title}
+                    authors={book.authors}
+                    description={book.description}
+                    thumbnail={book.thumbnail}
+                    onDelete={onDelete}
+                  />
+                </div>
+              ))}
+            </Slider>
           </CardContainer>
         </BooksArticlesSection>
-          <VideoSection>
-                <h2>Video</h2>
-                <CustomVideo src="https://www.youtube.com/embed/1Q8fG0TtVAY" type="iframe" />
-                <CustomVideo src="https://www.youtube.com/embed/1Q8fG0TtVAY" type="iframe" />
-            </VideoSection>
-          <ContextSection>
-              <h2>About the Ashram</h2>
-              <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed gravida
-              risus ac aliquet feugiat. Sed ullamcorper posuere orci, euismod
-              venenatis felis. Nunc eget tellus a velit bibendum vehicula ac eu
-              leo. Nulla facilisi. Donec blandit eleifend enim at blandit.{" "}
-              </p>
-          </ContextSection>
+        <VideoSection>
+          <h2>Video</h2>
+          <CustomVideo src="https://www.youtube.com/embed/1Q8fG0TtVAY" type="iframe" />
+          <CustomVideo src="https://www.youtube.com/embed/1Q8fG0TtVAY" type="iframe" />
+        </VideoSection>
+        <ContextSection>
+          <h2>About the Ashram</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed gravida
+            risus ac aliquet feugiat. Sed ullamcorper posuere orci, euismod
+            venenatis felis. Nunc eget tellus a velit bibendum vehicula ac eu
+            leo. Nulla facilisi. Donec blandit eleifend enim at blandit.{" "}
+          </p>
+        </ContextSection>
       </Main>
     </Container>
   );
