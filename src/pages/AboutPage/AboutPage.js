252e235f05465a77151d7939b0e08e91015f1568
Madan Gopal <madan.23.2@mountblue.tech>
2023-05-15 22:13:42 +0530

    Merge branch 'main' of https://github.com/jnvsumit/ashram-app into feat-ui

--- src/pages/AboutPage/AboutPage.js
+++ src/pages/AboutPage/AboutPage.js
@@ -10,7 +10,7 @@ function AboutPage() {
   return (
     <AboutContainer>
       <AboutInfo>
-        <h1>About Our Ashram</h1>
+        <h2>About Us</h2>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus
           magna eu quam consequat consequat. Vestibulum eu purus a ante
@@ -19,6 +19,7 @@ function AboutPage() {
           Vestibulum commodo elit vitae libero iaculis, id commodo elit
           pulvinar.
         </p>
+        <h2>Our Story</h2>
         <p>
           Curabitur non volutpat nunc. Sed auctor sapien vel lectus bibendum,
           vitae gravida nisi faucibus. Suspendisse vitae leo eu nibh volutpat
@@ -27,6 +28,24 @@ function AboutPage() {
           mauris convallis auctor. Vivamus quis purus a libero commodo dictum.
           Nunc fermentum, nulla id interdum ultricies, lorem sapien placerat
           justo, vel pulvinar odio orci a metus.
+        </p>
+        <h2>Our Mission</h2>
+        <p>
+          Curabitur non volutpat nunc. Sed auctor sapien vel lectus bibendum,
+          vitae gravida nisi faucibus. Suspendisse vitae leo eu nibh volutpat
+          molestie. Nam sit amet lorem a mi ultrices suscipit ut sed ex. Nulla
+          sit amet purus a est fringilla dapibus vel eu erat. Sed nec ex quis
+          mauris convallis auctor. Vivamus quis purus a libero commodo dictum.
+          Nunc fermentum, nulla id interdum ultricies, lorem sapien placerat
+          justo, vel pulvinar odio orci a metus.
+        </p>
+        <h3>Get in Touch</h3>
+        <p>
+          If you have any questions or comments, please don't hesitate to contact us. We'd love to hear from you!
+
+          Email: info@ourwebsite.com
+          Phone: 555-123-4567
+          Address: 123 Main Street, Anytown, USA
         </p>
       </AboutInfo>
       <AboutImageContainer>
--- src/pages/AboutPage/AboutPage.styles.js
+++ src/pages/AboutPage/AboutPage.styles.js
@@ -1,27 +1,28 @@
 import styled from "styled-components";
 
 export const AboutContainer = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-between;
-  margin: 50px auto;
-  margin-top: 10rem;
-  width: 80%;
-  max-width: 1000px;
+  display:flex;
+  align-items:center;
+  justify-content:space-between;
+  padding:40px;
+  gap:10px;
+  background-color:#f9f4f1;
+  font-family: 'Raleway', sans-serif;
+  
+  @media(max-width:1024px){
+    flex-direction:column;
+  }
 `;
 
 export const AboutInfo = styled.div`
-  flex: 1;
-  padding-right: 20px;
+  flex:2;
 `;
 
 export const AboutImageContainer = styled.div`
-  flex: 1;
-  padding-left: 20px;
 `;
 
 export const AboutImage = styled.img`
-  width: 100%;
-  height: auto;
   border-radius: 5px;
+  width:100%;
+
 `;
